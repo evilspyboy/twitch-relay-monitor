6f1a854ec3da5c2fadbfb747eea243c0d33beb17
Mohamed Hasan <mnt@plusmnt.com>
2020-07-14 22:52:57 +0300

    File from RPZ

--- helper.py
+++ helper.py
@@ -1,7 +1,21 @@
 import requests
 import datetime
 import json
-#Get token
+import random
+import scrollphathd
+import time
+import math
+from scrollphathd.fonts import font3x5
+# First you need to register you application to get Client id and client secret
+# https://dev.twitch.tv/docs/authentication#registration
+
+# Get app token to access twich API
+# https://dev.twitch.tv/docs/authentication#introduction
+# return auth. object or False if an error happen.
+
+# App access token used here because I only need to access public data not specific user data.
+# scope for channel:read:hype_train	only.
+# https://dev.twitch.tv/docs/authentication#scopes
 def get_token(client_id,client_secret,grant_type,scope):
 
 	url = "https://id.twitch.tv/oauth2/token?client_id="+ \
@@ -19,6 +33,36 @@ def get_token(client_id,client_secret,grant_type,scope):
 		print("get_token network error")
 		return False
 
+# Check if the token is valid no not.
+# return True if valid and False if invalid or network error
+# https://dev.twitch.tv/docs/authentication#validating-requests
+def is_valid_token(access_token):
+	
+	url = "https://id.twitch.tv/oauth2/validate"
+	headers = {
+	  'Authorization': 'OAuth '+access_token
+	}
+	try:
+		response = requests.request("GET", url, headers=headers,)
+		r=json.loads(response.text)
+		if "expires_in" in r :
+			if r['expires_in'] < 60*60*24: #1 day
+				return False
+		else:
+			return False
+	except requests.Timeout as err:
+		print("is_valid_token timeout")
+		return False
+	except Exception as e:
+		print("is_valid_token network error ")
+		return False	
+	return True
+
+
+# get last train information
+# https://dev.twitch.tv/docs/api/webhooks-reference/#topic-hype-train-event
+# user id is long value returned from get_broadcaster_id using username
+# return last hype train action or false if there is a problem
 def get_last_hype_train_action(client_id,access_token,user_id):
 	url = "https://api.twitch.tv/helix/hypetrain/events?broadcaster_id="+ \
 		user_id + \
@@ -37,6 +81,9 @@ def get_last_hype_train_action(client_id,access_token,user_id):
 		print("get hype train action network error ")
 		return False
 
+# get broadcaster id from its username
+# https://dev.twitch.tv/docs/v5/reference/users/#get-users
+# return user object 
 
 def get_broadcaster_id(client_id,username):
 	url = "https://api.twitch.tv/kraken/users?login="+username
@@ -55,6 +102,9 @@ def get_broadcaster_id(client_id,username):
 		print("get_broadcaster_id network error ")
 		return False
 
+# check if tain is active by comparing current UTC time with hype train `expires_at`
+# return True if the train active, return False if the train end.
+
 def is_train_active(train_data):
 	# pprint.pprint(train_data)
 	# exit()
@@ -65,6 +115,10 @@ def is_train_active(train_data):
 	if datetime.datetime.utcnow() > event_end_date_time:
 		return False
 	return True
+
+# check if the user streaming
+# https://dev.twitch.tv/docs/api/reference#get-streams
+# return True if the user streaming, return False if the user offline
 
 def is_user_live(client_id,access_token,username):
 	import requests
@@ -87,25 +141,134 @@ def is_user_live(client_id,access_token,username):
 		print("can not check if user live ")
 		return False
 
-def is_valid_token(access_token):
-	
-	url = "https://id.twitch.tv/oauth2/validate"
-	headers = {
-	  'Authorization': 'OAuth '+access_token
-	}
-	try:
-		response = requests.request("GET", url, headers=headers,)
-		r=json.loads(response.text)
-		if "expires_in" in r :
-			if r['expires_in'] < 60*60*24: #1 day
-				return False
-		else:
-			return False
-	except requests.Timeout as err:
-		print("is_valid_token timeout")
-		return False
-	except Exception as e:
-		print("is_valid_token network error ")
-		return False	
-	return True
-	
+
+
+# LED functions
+
+# this function will run when the train is not active.
+# show random led pixel in random x,y location with brighness range from 0.1 to 0.5
+# the function show or hide one pixel first, then hide random pixel
+
+def rand_pixel():
+    x =random.randint(1,15)
+    y =random.randint(1,5)
+    b =random.randint(0,5)/10
+    scrollphathd.set_pixel(x,y,b)
+    x =random.randint(1,15)
+    y =random.randint(1,5)
+    scrollphathd.set_pixel(x,y,0)
+    scrollphathd.show()
+    
+
+# get location of the number in the rectangle watch
+# giving sx as start X point and n as a number
+# return x and y location 
+    
+def get_loc(sx,n):
+    if n ==12:
+        n=0
+    if n ==0 or n == 6:
+        y=1
+        if n ==6:
+            y =5
+        return sx+1,y
+    if n <= 5:
+        y =n
+        x=sx+2
+        return x,y
+    if n >=7:
+        y =12-n
+        x =sx
+        return x,y
+
+# Cooldown
+# show 12 base watch countdown after the train end.
+# based on this project: https://github.com/plusmnt/rectangle-scrollphathd-watch
+# take future as datetime object and subtract it from utc now
+# the output will be shown in the scrollphathd LED
+
+def show_time(future):
+    now=datetime.datetime.utcnow()
+    diff = future-now
+    print("Cooldown timer:",diff)
+    time_str=str(diff).split(":")
+
+    #Draw 3 rectangles background
+    #sq: will give 1, 7, 13 as x starting point
+    for sq in range(1, 15, 6):
+        for sx in range(sq,sq+3):
+            for sy in range(1,6):
+                if sx ==sq+1 :
+                    if sy !=1 and sy !=5:
+                        continue
+                scrollphathd.set_pixel(sx,sy,0.13)
+
+#Draw ':'
+    scrollphathd.set_pixel(5,2,0.13)
+    scrollphathd.set_pixel(5,4,0.13)
+    scrollphathd.set_pixel(11,2,0.13)
+    scrollphathd.set_pixel(11,4,0.13)
+
+    #set time
+    second_12_base=int(float(time_str[2])/5+0.5)
+    sec_x,sec_y=get_loc(13,second_12_base)
+    scrollphathd.set_pixel(sec_x,sec_y,0.5)
+    
+    minute_12_base=int(int(time_str[1])/5+0.5)
+    if int(time_str[1]) != 0:
+        min_x,min_y=get_loc(7,minute_12_base)
+        scrollphathd.set_pixel(min_x,min_y,0.5)
+
+    hour=int(time_str[0])
+    if hour !=0:
+        if hour >12:
+            hour=hour-12
+        hour_x,hour_y=get_loc(1,hour)
+        scrollphathd.set_pixel(hour_x,hour_y,0.5)
+    scrollphathd.show()
+
+
+# Active train function
+# show the prorgress and the level of hype train.
+# the funcation hype train level, pre as percentage (goal/total),  previous x value and previous y value.
+# from 1 to 11 x value it will show the progress of the train.
+# if the train pass 100% the led will stop in the last level 5 dot.
+# the output will be shown in the scrollphatHD led display.
+
+def progress_show(level,per,prev_x,prev_y):
+
+    #max value for x in the led display
+    max_x=11
+    fix_x=prev_x
+    fix_y=prev_y
+    # get the upper value of the percentage e.g. 4.1 = 5
+    cal_x = math.ceil(per /10)
+    print("Cal_x:",cal_x)
+    if cal_x >=max_x:
+        cal_x=max_x
+    for y in range(fix_y,level,1):
+        x_limit=max_x
+        if y == level-1:
+            x_limit=cal_x+1
+            if x_limit >= max_x:
+                x_limit =max_x
+        for x in range(fix_x,x_limit):
+            if prev_x !=1 or prev_y !=5 :
+                scrollphathd.set_pixel(prev_x,5-prev_y,0.1)
+            scrollphathd.set_pixel(x,5-y,0.3)
+            scrollphathd.clear_rect(x=13,y=1,width=3,height=5)
+            scrollphathd.write_string(str(y+1),x=13, y=1, font=font3x5, brightness=0.3)
+            scrollphathd.show()
+            time.sleep(0.1)
+            prev_x=x
+            prev_y=y
+        
+        fix_x=1
+
+    return prev_x,prev_y
+
+# clear ScrollpHatHD led display
+def clear_pixel():
+    scrollphathd.clear()
+    scrollphathd.show()
+
--- run.py
+++ run.py
@@ -4,20 +4,38 @@ from config import *
 from helper import *
 import time
 
+#first start by getting token to access twitch api
 r=get_token(client_id,client_secret,grant_type,scope)
 if r == False:
+    # if there is a problem, end the program
 	print("Can't Auth user")
 	exit(1)
 
+# since streamer username is given we need to get its broadcaster id for other requests
 broadcaster=get_broadcaster_id(client_id,username)
 if broadcaster==False:
+    # if there is a problem, end the program
 	print("Can not get broadcster id")
 	exit(1)
+
+if "access_token" not in r:
+    # if there is a problem, end the program
+	print("Access token is missing ", r)
+	exit(1)
+	
 access_token=r['access_token'];
 expires_in=r['expires_in']
-fresh_token_interval=token_validate_interval
 
+# fresh token interval will keep track of the time we need to validate the token
+fresh_token_interval=token_validate_interval
 skip_count=0
+wait_type="nt" #nt: no hype train, cd: cooldown, at: active hype train
+# store old value of wait_type to clear the LED display when the new event happen.
+prev_wait_type="nt"
+#for active train 
+prev_x=1
+prev_y=0
+
 while True:
 	wait_time=online_user_wait_time
 	
@@ -28,7 +46,7 @@ while True:
 			r=get_token(client_id,client_secret,grant_type,scope)
 			if r ==False:
 				skip_count=skip_count+1
-				print("Skipp get token , skip:", skip_count)
+				print("Skip get token , skip:", skip_count)
 				time.sleep(skip_wait_time)
 				continue
 			access_token=r['access_token'];
@@ -40,22 +58,30 @@ while True:
 		print("User ["+username+"] online")
 	else:
 		print("User ["+username+"] offline")
-		wait_time=60
+		wait_time=user_offline_wait_time
 
 	last_hype_train_action=get_last_hype_train_action(client_id,access_token,broadcaster["_id"])
 	if last_hype_train_action ==False:
 		skip_count=skip_count+1
-		print("Skipp get token , skip:", skip_count)
+		print("Skip get token , skip:", skip_count)
 		time.sleep(skip_wait_time)
 		continue
+		
 	if(is_train_active(last_hype_train_action["data"])):
 		#pprint.pprint(last_hype_train_action["data"])
 		precent=int(round(last_hype_train_action["data"][0]["event_data"]['total']*100/last_hype_train_action["data"][0]["event_data"]['goal']))
 		print("Train Active at level ",last_hype_train_action["data"][0]["event_data"]['level'])
 		print("Level complete %: ",precent)
-		wait_time=5
+		wait_type="at"
+		level=last_hype_train_action["data"][0]["event_data"]['level']
+		# clear ScrollpHatHD display before showing the progress
+		if prev_wait_type !="at":
+			clear_pixel()
+		prev_x,prev_y=progress_show(level,precent,prev_x,prev_y)
+		wait_time=5 # active hype train wait time in seconds
 	else:
 		print("Train not active")
+		wait_type="nt"
 		wait_time=online_user_wait_time
 	#check hype train cooldown_end_time
 	if "data" in last_hype_train_action:
@@ -63,15 +89,42 @@ while True:
 			cooldown_end_time=last_hype_train_action["data"][0]["event_data"]["cooldown_end_time"]
 			cooldown_end_time=datetime.datetime.strptime(cooldown_end_time,'%Y-%m-%dT%H:%M:%SZ')
 			if cooldown_end_time > datetime.datetime.utcnow() and is_train_active(last_hype_train_action["data"])==False:
-				if int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds()-60*5) >0:
-					wait_time=int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds()-60*5) #5min padding
-					print("Cool down end:",int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds()-60*5) )
+				if int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds()-5*60) >0:
+					wait_time=int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds())-5*60 
+					print("Cool down end:",int((cooldown_end_time - datetime.datetime.utcnow()).total_seconds())-5*60 )
+					wait_type="cd"
 	fresh_token_interval=fresh_token_interval-wait_time
 
 	if skip_count == max_skip_count:
 		print("Skip count limit reached")
 		exit(1)
 
+	#print("wait_type:",wait_type)
+	if wait_type == "cd":
+		if prev_wait_type !="cd":
+			prev_x=1
+			prev_y=0
+			clear_pixel()
+		while wait_time > 0:
+			next_hype_train=datetime.datetime.strptime(last_hype_train_action["data"][0]["event_data"]["cooldown_end_time"]  ,'%Y-%m-%dT%H:%M:%SZ')
+			if datetime.datetime.utcnow() >= next_hype_train:
+			    break 
+			show_time(next_hype_train)
+			time.sleep(1)
+			wait_time =wait_time-1
+	elif wait_type == "nt":
+		if prev_wait_type !="nt":
+			prev_x=1
+			prev_y=0
+			clear_pixel()
+		while wait_time >0:
+			rand_pixel()
+			time.sleep(1)
+			wait_time =wait_time-1
+	
+	prev_wait_type=wait_type	    
 	time.sleep(wait_time)
+	#reset skip_count if one request execute without issue within max_skip_count
+	skip_count=0 
 
 

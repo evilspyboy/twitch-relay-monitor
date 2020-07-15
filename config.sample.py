e60ad7643a235428ca0acc8afe32a0f3479befef
Mohamed Hasan <mnt@plusmnt.com>
2020-07-15 19:31:57 +0300

    fix comments

--- config.sample.py
+++ config.sample.py
@@ -13,7 +13,7 @@ grant_type="client_credentials"
 # https://dev.twitch.tv/docs/authentication#scopes
 scope="channel:read:hype_train"
 
-# channel user name 
+# channel username 
 username=""
 
 # time to wait between request if the user not streaming
@@ -25,8 +25,8 @@ online_user_wait_time=30 #30 seconds.
 # time to check if the token is valid
 token_validate_interval=60*60*1 #1 hour
 
-# maximum number of skip when there is a problem before the program stop
+# maximum number of skipping when there is a problem before the program stop
 max_skip_count=5
 
-# cooldown time before sending new request when an error heppen.
+# cooldown time before sending a new request when an error happens.
 skip_wait_time=5 #5 seconds
